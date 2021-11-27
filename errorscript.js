4da2268d38ad6249a72a863cb4665886dae9aa96
Piyush <piyushkr28@outlook.com>
2021-11-27 15:49:58 +0530

    updated function after scanning

--- errorscript.js
+++ errorscript.js
@@ -1,67 +1,78 @@
-function One(x) { // ARRAY_INDEX_NEGATIVE
-    var arr = [1, 2, 3];
-    if (x < 0) {
-        arr[x] = 3;
-    }
+function One(x) {
+  // ARRAY_INDEX_NEGATIVE
+  var arr = [1, 2, 3];
+  if (x > 0) {
+    arr[x] = 3;
+  }
 }
 
-function Two() { // IDENTICAL_BRANCHES
-    if (x >= 0) {
-        y = x;
-    } else {
-        y = x;
-    }
+function Two() {
+  // IDENTICAL_BRANCHES
+  if (x >= 0) {
+    y = x;
+  }
+  //else {
+  //     y = x;
+  // }
 }
 
-function Three() { // NULL_POINTER
-    var obj;
-    var y = obj.x;
-    console.log(y);
+function Three() {
+  // NULL_POINTER
+  var obj = 0;
+  var y = obj.x;
+  console.log(y);
 }
 
-function Four() { // BAD_BITWISE_OPERATOR
-    var obj = null;
-    if (obj & obj.prop) {
-        console.log(obj.prop);
-    }
+function Four() {
+  // BAD_BITWISE_OPERATOR
+  var obj = 0;
+  if (obj && obj.prop) {
+    console.log(obj.prop);
+  }
 }
 
-function Five() { // BAD_ASSIGN_IN_CONDITION
-    var x = -1;
-    if (x = -1) console.log('Error!', x);
+function Five() {
+  // BAD_ASSIGN_IN_CONDITION
+  var x = -1;
+  if ((x = -1)) console.log("Error!", x);
 }
 
-function Six() { // UNUSED_EXPR
-    this.foo + 42;
-}//
+function Six() {
+  // UNUSED_EXPR
+  this.foo + 42;
+} //
 
-function Seven() { // SWITCH_CASE_FALL_THROUGH
-    var x;
-    switch (x) {
-    case '1': console.log('Do one thing');
-    case '2': console.log('Do another thing');
-    }
+function Seven() {
+  // SWITCH_CASE_FALL_THROUGH
+  var x;
+  switch (x) {
+    case "1":
+      console.log("Do one thing");
+    case "2":
+      console.log("Do another thing");
+  }
 }
 
-function Eight() { // CONSTANT_CONDITION
-    var x = "Fo";
-    var y = x + "Bar";
-    if (y === "FooBar") {
-        console.log('Do something');
-    }
+function Eight() {
+  // CONSTANT_CONDITION
+  var x = "Fo";
+  var y = x + "Bar";
+  if (y === "FooBar") {
+    console.log("Do something");
+  }
 }
 
-function Nine() { // UNREACHABLE_CODE
-    return;
+function Nine() {
+  // UNREACHABLE_CODE
+  return;
 
-    var bar = 1;
-    console.log(bar);
+  var bar = 1;
+  console.log(bar);
 }
 
-function Ten(url) { // UNUSED_VAR_ASSIGN
-    var foo = url + "?query=deepscan";
-    foo = url.replace(/\s/gi, "");
-    console.log(foo);
+function Ten(url) {
+  // UNUSED_VAR_ASSIGN
+  var foo = url + "?query=deepscan";
+  foo = url.replace(/\s/gi, "");
+  console.log(foo);
 }
-
-
